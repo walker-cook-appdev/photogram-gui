be6f40f7ac43299857f356d9885f0b46d05e206b
walker-cook <jwcook99@gmail.com>
2022-02-15 20:16:29 +0000

    Update

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   get("/users/:path_username", {:controller => "users", :action => "show"})
   get("/photos", {:controller => "photos", :action => "index"})
   get("/photos/:path_id", {:controller => "photos", :action => "show"})
+  get("/delete_photo/:path_id", {:controller => "photos", :action => "delete"})
 end
